d895af2fed8ed61cf718b957b17719a3d5d5c6ff
Richard Gray <richard@crash.net.nz>
2015-05-16 20:06:53 +1200

    Create HAProxy listener from Hiera.

--- manifests/haproxy.pp
+++ manifests/haproxy.pp
@@ -1,3 +1,9 @@
 class profiles::haproxy {
 
+  $my_haproxy_listeners = hiera('haproxy::listeners', {})
+  create_resources('haproxy::listen', $my_haproxy_listeners)
+
+  $my_haproxy_balancermembers = hiera('haproxy::balancermembers', {})
+  create_resources('haproxy::balancermember', $my_haproxy_balancermembers)
+
 }
